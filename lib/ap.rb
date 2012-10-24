f789e733623f1446b86ae5d9b08dfb77c27da21d
Mike Dvorkin <mike@dvorkin.net>
2012-10-23 17:17:58 -0700

    Release 0.1.0

---
+++ lib/ap.rb
@@ -0,0 +1,9 @@
+# Copyright (c) 2010-2012 Michael Dvorkin
+#
+# Awesome Print is freely distributable under the terms of MIT license.
+# See LICENSE file or http://www.opensource.org/licenses/mit-license.php
+#------------------------------------------------------------------------------
+#
+# Keeping this to allow require "ap"
+#
+require File.dirname(__FILE__) + "/awesome_print_motion"
--- test/app/app_delegate.rb
+++ test/app/app_delegate.rb
@@ -5,7 +5,7 @@ class AppDelegate
     # Assertion failed: (b != NULL), function rb_vm_block_method_imp, file vm.cpp, line 3000.
     #
     quickie do
-      1.should == 1   
+      1.should == 1
     end
     true
   end
